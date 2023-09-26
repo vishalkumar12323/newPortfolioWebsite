f0df2a322b1ed65f6c019ee3606cd7460cba1400
vishal <vishalninaniya8949info@gmail.com>
2023-09-26 23:34:05 +0530

    updated javascript code with functions

--- script/script.js
+++ script/script.js
@@ -1,46 +1,64 @@
-const heroSection = document.querySelector('.hero-section');
-const imgSection = document.querySelector('.images-section');
-const header = document.querySelector('.header');
+const heroSection = document.querySelector(".hero-section");
+const imgSection = document.querySelector(".images-section");
+const header = document.querySelector(".header");
 
-// Create Sticky Navbar =========================================>Start
-const newObserver = new IntersectionObserver((ent) => {
-    const entries = ent[0];
-    // console.log(entries);
+// Create Sticky Navbar ========>
+const stickyNavbar = () => {
+  const newObserver = new IntersectionObserver(
+    (ent) => {
+      const entries = ent[0];
+      // console.log(entries);
 
-    if (!entries.isIntersecting) {
-        document.body.classList.add('sticky');
-    }
-    else {
-        document.body.classList.remove('sticky');
-    }
+      if (!entries.isIntersecting) {
+        document.body.classList.add("sticky");
+      } else {
+        document.body.classList.remove("sticky");
+      }
 
+      if (!entries.isIntersecting) {
+        document.body.classList.remove("scrollBtnHide");
+      } else {
+        document.body.classList.add("scrollBtnHide");
+      }
+    },
+    { root: null, threshold: 0 }
+  );
+  newObserver.observe(heroSection);
+};
+stickyNavbar();
 
-    if (!entries.isIntersecting) {
-        document.body.classList.remove('scrollBtnHide');
-    }
-    else {
-        document.body.classList.add('scrollBtnHide');
-    }
-}, { root: null, threshold: 0 });
-newObserver.observe(heroSection);
-// Create Sticky Navbar =========================================>End.
+// Toggling Responsive Navbar =========>
+const toggleNavbar = () => {
+  const mobileBar = document.querySelector(".mobile-bar-icon");
 
+  const mobileNavbar = () => {
+    header.classList.toggle("active");
+  };
+  mobileBar.addEventListener("click", mobileNavbar);
+};
+toggleNavbar();
 
-
-// Toggling Responsive Navbar =========================================>Start
-const mobileBar = document.querySelector('.mobile-bar-icon');
-
-const mobileNavbar = () => {
-    header.classList.toggle('active');
-}
-mobileBar.addEventListener('click', mobileNavbar);
-// Toggling Responsive Navbar =========================================>End.
-
-
-// Click ScrollToTop Functionality =========================================>Start
-const scrollBtn = document.querySelector('.scrollBtn');
-
-scrollBtn.addEventListener('click', () => {
+// Click ScrollToTop Functionality =======>
+const clickToScrollTop = () => {
+  const scrollBtn = document.querySelector(".scrollBtn");
+  scrollBtn.addEventListener("click", () => {
     heroSection.scrollIntoView({ behavior: "smooth" });
-});
-// Click ScrollToTop Functionality =========================================>End.
+  });
+};
+clickToScrollTop();
+
+const onMouseAnimation = () => {
+  const images_section = document.querySelector("#images-section");
+  images_section.addEventListener("mousemove", (dets) => {
+    gsap.to("#cursor_anim", {
+      left: dets.x,
+      top: dets.y,
+    });
+  });
+};
+onMouseAnimation();
+const offMouseAnimation = () => {
+  const images_section = document.querySelector("#images-section");
+  images_section.addEventListener("mouseleave", () => {});
+};
+offMouseAnimation();
